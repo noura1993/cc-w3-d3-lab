9618f1a7553522f0ee334d47a6018115799f06f6
Nourhan Elhakim <nourhanmagdy993@gmail.com>
2020-05-27 15:26:38 +0100

    Create new instance in console

---
+++ console.rb
@@ -0,0 +1,50 @@
+require('pry')
+require_relative('../models/artist')
+require_relative('../models/album')
+
+# Album.delete_all
+# Artist.delete_all
+
+katy = Artist.new({
+    'name' => 'Katty Perry'
+})
+
+# katy.save()
+
+taylor = Artist.new({
+    'name' => 'Taylor Swift'
+})
+
+# taylor.save()
+
+prism = Album.new({
+    'title' => 'Prism',
+    'genre' => 'pop',
+    'artist_id' => katy.id
+})
+
+# prism.save()
+
+witness = Album.new({
+    'title' => 'witness',
+    'genre' => 'Electropop',
+    'artist_id' => katy.id
+})
+
+# witness.save()
+
+fearless = Album.new({
+    'title' => 'Fearless',
+    'genre' => 'Country Pop',
+    'artist_id' => taylor.id
+})
+
+# fearless.save()
+
+lover = Album.new({
+    'title' => 'Lover',
+    'genre' => 'Pop',
+    'artist_id' => taylor.id
+})
+
+# lover.save()
