2bd5e131fbe7793c06a018b83dfc94e83de045f2
Nourhan Elhakim <nourhanmagdy993@gmail.com>
2020-05-27 14:52:50 +0100

    Initial commit

---
+++ db/music.sql
@@ -0,0 +1,18 @@
+DROP TABLE IF EXISTS albums;
+DROP TABLE IF EXISTS artists;
+
+
+CREATE TABLE artists (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255)
+);
+
+
+CREATE TABLE albums (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255),
+    genre VARCHAR(255),
+    artistist_id REFERENCES artists (id)
+
+);
+
